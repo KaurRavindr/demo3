571a1110fb3fa68900a2ae82bc992b64aa954b98
ravin <ravin@DESKTOP-6MTLT29.lan>
2023-01-10 21:50:16 -0500

    commit

---
+++ src/com/noorteck/git/raavi/Main.java
@@ -0,0 +1,10 @@
+package com.noorteck.git.raavi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("git by Raavi");
+	}
+
+}
